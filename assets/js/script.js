ff31059d248b2b57d50106e73d17006ab4e41c34
emma-nkn <emmacarene7@gmail.com>
2025-12-07 18:07:09 +0100

    Mise à jour section innovation carrousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,21 @@
-new Swiper(" .card-wrapper", {
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
   loop: true,
-  spaceBetween: 40,
-  slidesPerView: 3,
+  grabCursor: true,
 
   pagination: {
-    el: " .swiper-pagination",
+    el: ".swiper-pagination",
     clickable: true,
   },
 
-  //Navigation arrows
   navigation: {
-    nextEl: " .swiper-button-next",
-    prevEl: " .swiper-button-prev",
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
-  // responsive breakpoints
   breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
+    576: { slidesPerView: 2 },
+    992: { slidesPerView: 3 },
   },
 });
-
-{
-}
