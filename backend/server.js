43ba9632f82b52ebe6044ef248152c53946175a1
emma-nkn <emmacarene7@gmail.com>
2025-12-25 00:58:06 +0100

    Initialisation de l'environnement backend avec installation de Express.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("moes premier pas en nodejs");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🚀 Mon premier serveur Node.js fonctionne !");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
+});
